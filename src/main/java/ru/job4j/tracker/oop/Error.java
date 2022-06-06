d93f4aea6f2f7494b598bad6a8a0ae03deda7d4c
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-06 07:36:47 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private int status;
+    private boolean active;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+    this.active = active;
+    this.status = status;
+    this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error stroka1 = new Error(true, 1, "Hello");
+        stroka1.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+}
