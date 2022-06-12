9ccb8da86f9784432c318af52e928e35d589e3bd
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-12 13:08:21 +0300

    6. Аннотация @Override [#174891]

---
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class HtmlReport extends Techport {
+
+    @Override
+    public String generate( String name, String body) {
+        return "<h1>" + name +  "</h1>" +
+                "<br1>" +
+                "<span>" + body + "</span>";
+    }
+}
