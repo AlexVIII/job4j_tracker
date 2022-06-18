f061476cddd2cf5d41bbb0b8f9f83812ee5388e5
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-18 06:55:18 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item timeFormat = new Item();
-        System.out.println(timeFormat.getTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+        System.out.println(timeFormat.getTime().
+                format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+        System.out.println(timeFormat);
     }
 }
