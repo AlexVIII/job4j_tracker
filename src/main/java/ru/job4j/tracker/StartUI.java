e748f28396aec6ebb60d81cb20e42bdaa997adad
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-20 07:57:42 +0300

    1. Чтение из консоли. Класс Scanner. [#179514]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,40 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
-    public static void main(String[] args) {
-        Item timeFormat = new Item();
-        System.out.println(timeFormat.getTime().
-                format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
-        System.out.println(timeFormat);
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu(scanner);
+        }
+        System.out.print("Select: ");
+        int select = Integer.parseInt(scanner.nextLine());
+        if (select != 6) {
+            System.out.println("Пользователь выбрал :" +  select);
+        } else {
+            run = false;
+        }
+
     }
+
+    private  void showMenu(Scanner scanner) {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
+
 }
