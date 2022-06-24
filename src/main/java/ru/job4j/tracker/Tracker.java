8cbef4b03fbb62982989739c6c6be57066f567ae
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-24 04:44:53 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322 #315534

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -23,7 +23,7 @@ public class Tracker {
         int count = 0;
         for (int i = 0; i < size; i++) {
             Item item = items[i];
-            if (item.getName().contains(key)) {
+            if (item.getName().equals(key)) {
                 rsl[count] = item;
                 count++;
             }
