e5bce10b639f649b89897cf6e6085b979c8def40
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-28 22:09:47 +0300

    5. Input и полиморфизм. [#181094]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ConsoleInput implements Input {
     private final Scanner scanner = new Scanner(System.in);
 
-
     @Override
     public String askStr(String question) {
         System.out.print(question);
