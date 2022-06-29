81ec4a18d455f382b9805850a5d82d9bc8ab1ebe
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-06-29 12:47:32 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,8 +1,10 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
 
 public class StartUITest {
 
@@ -16,4 +18,27 @@ public class StartUITest {
         Item expected = new Item("Fg");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void editItem() {
+        Tracker tracker = new Tracker();
+        Item item =  new Item("new Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "edited item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "Данной заявки нет"};
+        StartUI.delete(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted.getName());
+    }
+
 }
