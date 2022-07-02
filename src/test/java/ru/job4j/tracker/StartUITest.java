9bf2f5ad8d2bd848873a4350e956d7774cf76d6d
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-07-02 06:04:03 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,55 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertNull;
+public class StartUITest {
+
+    @Test
+    public void createItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced Item"));
+        String replacedName = "New item name";
+        Input in = new StubInput(
+                new String[]{"0", "New item name", "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker =  new Tracker();
+        Item item = tracker.add(new Item("deleted item"));
+        Input in = new StubInput(
+                new String[] {"0", "deleted item", "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+}
