fb4f4b13ac0d9fe6d752e5841f53ea6b7133155e
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-07-07 05:52:32 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,22 +1,7 @@
 package ru.job4j.ex;
 
-public class ElementNotFoundException {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException1 {
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
-            }
-        }
-        return rsl;
-    }
-
-    public static void main(String[] args) throws ElementNotFoundException1 {
-        try {
-            System.out.println(indexOf(new String[]{"l", "k", "m"}, "m"));
-            System.out.println(indexOf(new String[]{"l", "k", "u"}, "0"));
-        } catch (ElementNotFoundException1 e) {
-            e.printStackTrace();
-        }
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
     }
 }
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element is not find");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"l", "k", "m"}, "m"));
+            System.out.println(indexOf(new String[]{"l", "k", "u"}, "0"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
