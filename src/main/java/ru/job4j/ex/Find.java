0e154cea4213cbcf7a390adb9731a5e3c30bec3b
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-07-09 22:59:13 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/main/java/ru/job4j/ex/Find.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+public class Find {
+    public static String get(String[] data, int index) {
+        if (index >= data.length) {
+            throw   new IllegalArgumentException("Index out of bound of Array");
+        }
+        return data[index];
+    }
+
+    public static void main(String[] args) {
+        String[] data = {"one", "two", "three"};
+        String rsl = Find.get(data, 7);
+        System.out.println(rsl);
+    }
+}
