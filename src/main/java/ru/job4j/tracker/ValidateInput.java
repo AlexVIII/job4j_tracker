f2c108720df42873a10b7a9cfca02a303c14d343
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-07-11 05:52:39 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117 #320338]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -10,18 +10,6 @@ public class ValidateInput implements Input {
         this.in = input;
     }
 
-    private boolean isNumber(String value) {
-        boolean rsl = true;
-        char[] check = value.toCharArray();
-        for (char num : check) {
-            if (num < 48 || num > 57) {
-                rsl = false;
-                break;
-            }
-        }
-        return rsl;
-    }
-
     @Override
     public String askStr(String question) {
         return in.askStr(question);
@@ -32,13 +20,13 @@ public class ValidateInput implements Input {
         boolean invalid = true;
         int value = -1;
         do {
-            String rsl = in.askStr(question);
-            if (!isNumber(rsl)) {
-                System.out.println("PLEASE ENTER VALIDATE AGAIN");
-                continue;
+
+            try {
+                value = in.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
             }
-            value = Integer.parseInt(rsl);
-            invalid = false;
         } while (invalid);
         return value;
     }
