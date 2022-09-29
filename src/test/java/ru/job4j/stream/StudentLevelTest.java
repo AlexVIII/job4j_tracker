21fad93206216c57676f2ace7cd80118848245c2
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-09-29 06:27:10 +0300

    0. Stream API улучшения [#58167]

---
+++ src/test/java/ru/job4j/stream/StudentLevelTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StudentLevelTest {
+
+    @Test
+    public void whenSorted() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student("Misha", 26));
+        input.add(new Student("Masha", 78));
+        List<Student> expect  = List.of(
+                new Student("Misha", 26),
+                new Student("Masha", 78)
+        );
+        assertThat(StudentLevel.levelOf(input, 20)).containsSequence(expect);
+    }
+
+    @Test
+    public void whenOnlyNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        List<Student> expect  = List.of();
+        assertThat(StudentLevel.levelOf(input, 20)).containsSequence(expect);
+    }
+
+    @Test
+    public void whenHasNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student("Misha", 26));
+        input.add(new Student("Masha", 78));
+        input.add(null);
+        List<Student> expect  = List.of(
+                new Student("Misha", 26),
+                new Student("Masha", 78)
+        );
+        assertThat(StudentLevel.levelOf(input, 20)).containsSequence(expect);
+    }
+}
