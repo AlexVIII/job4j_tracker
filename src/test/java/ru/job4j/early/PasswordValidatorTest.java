7358b4b7c462b4baf5a4c294dd3231d85fcaa668
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-10-11 06:59:00 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/test/java/ru/job4j/early/PasswordValidatorTest.java
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -1,60 +1,41 @@
 package ru.job4j.early;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+public class PasswordValidatorTest {
 
-class PasswordValidatorTest {
-
-    @Test
-    void whenPasswordNotHaveDigital() {
-        String result = PasswordValidator.validate("hJ*ggggI");
-        String expect = "the password must contain at least one digit,"
-                + "a lowercase or uppercase character, and special characters.";
-        assertThat(result).isEqualTo(expect);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordNotHaveDigital() {
+        PasswordValidator.validate("hJ*ggggI");
     }
 
-    @Test
-    void whenPasswordNotHaveUpperCharacter() {
-        String result = PasswordValidator.validate("h1*ggggI");
-        String expect = "the password must contain at least one digit,"
-                + "a lowercase or uppercase character, and special characters.";
-        assertThat(result).isEqualTo(expect);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordNotHaveUpperCharacter() {
+       PasswordValidator.validate("h1*ggggI");
     }
 
-    @Test
-    void whenPasswordNotHaveLowerCharacter() {
-        String result = PasswordValidator.validate("1J*GGGGI");
-        String expect = "the password must contain at least one digit,"
-                + "a lowercase or uppercase character, and special characters.";
-        assertThat(result).isEqualTo(expect);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordNotHaveLowerCharacter() {
+        PasswordValidator.validate("1J*GGGGI");
     }
 
-    @Test
-    void whenPasswordIsShort() {
-        String result = PasswordValidator.validate("1hJ*I");
-        String expect = "Password is short or long";
-        assertThat(result).isEqualTo(expect);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordIsShort() {
+        PasswordValidator.validate("1hJ*I");
+        }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordIsLong() {
+       PasswordValidator.validate("1hhhhhhhhhhhhhhhhhhhhhhhhhhhhhJ*I");
     }
 
-    @Test
-    void whenPasswordIsLong() {
-        String result = PasswordValidator.validate("1hhhhhhhhhhhhhhhhhhhhhhhhhhhhhJ*I");
-        String expect = "Password is short or long";
-        assertThat(result).isEqualTo(expect);
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPasswordHaveUsed() {
+       PasswordValidator.validate("admin1J*GGhGI");
     }
 
-    @Test
-    void whenPasswordHaveUsed() {
-        String result = PasswordValidator.validate("admin1J*GGhGI");
-        String expect = "Password is used in base";
-        assertThat(result).isEqualTo(expect);
-    }
-
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void whenEmpty() {
-        String result = PasswordValidator.validate("");
-        String expect = "Password is used";
-        assertThat(result).isEqualTo(expect);
+       PasswordValidator.validate("");
     }
 }
